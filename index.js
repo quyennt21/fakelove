6dd3910c86f40d681691e84aa5eb30d78773862b
Quyen <ken.tb997@gmail.com>
2019-08-18 00:21:38 +0700

    commit code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 /** Init express app */
 const app = express();
-app.use(express.static('/3d_box'))
+app.use(express.static('3d_box'))
 app.listen(3000, () => {
     console.log(`Server running on port 3000.`);
 });
